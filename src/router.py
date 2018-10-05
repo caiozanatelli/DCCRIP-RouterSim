02f74f3cd1c351dad6c6e95704f4ccc699e48b82
Breno <brenoaugmartins@gmail.com>
2018-10-05 01:42:56 -0300

    Message subclasses

--- src/router.py
+++ src/router.py
@@ -18,6 +18,47 @@ class Message:
 		self.__dest		= dest
 		self.__type		= msg_type
 
+	def to_dict(self):
+		d = dict()
+		d["type"] = self.__type
+		d["source"] = self.__src
+		d["desination"] = self.__dest
+		return d
+
+class Data(Message):
+	__payload	= None
+
+	def __init__(self, src, dest, msg_type, payload):
+		Message.__init__(self, src, dest, msg_type)
+		self.__payload		= payload
+
+	def to_dict(self):
+		d = super().to_dict(self)
+		d["payload"] = self.__payload
+		return d
+
+class Update(Message):
+	__distances	= None
+
+	def __init__(self, src, dest, msg_type, distances):
+		Message.__init__(self, src, dest, msg_type)
+		self.__distances	= distances
+
+	def to_dict(self):
+		d = super().to_dict(self)
+		d["distances"] = self.__distances
+		return d
+
+class Trace(Message):
+	__hops	= None
+
+	def __init__(self, src, dest, msg_type, hops):
+		Message.__init__(self, src, dest, msg_type)
+		self.__hops	= hops
+
+	d = super().to_dict(self)
+		d["hops"] = self.__hops
+		return d
 
 class Packet:
 	def json_encoding(json_dict):
@@ -27,6 +68,15 @@ class Packet:
 	def json_decoding(json_str):
 		json_dict = json.loads(json_str)
 		return json_dict
+
+	def to_struct(json_str):
+		data = bytes(json_str, 'ascii')
+		return data
+
+	def to_string(data):
+		json_str = data.decode('ascii')
+		return json_str
+
 
 class Router:
 	__ip		= None
