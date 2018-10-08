f94fa146dd781fd4a75f75991c50c4ad54d17b3b
Caio Zanatelli <caio.zanatelli@dcc.ufmg.br>
2018-10-08 16:41:36 -0300

    Refactoring the command handler

--- src/router.py
+++ src/router.py
@@ -12,43 +12,50 @@ class Router:
     __sock   = None
 
     def __init__(self, ip, period):
-        self.__ip       = ip
-        self.__period   = period
-        self.__port     = DEFAULT_PORT
-        self.__sock     = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__ip     = ip
+        self.__period = period
+        self.__port   = DEFAULT_PORT
+        self.__sock   = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__sock.bind((self.__ip, self.__port))
 
     def run(self):
         while True:
-            cmd    = raw_input().split(' ')
-            length =  len(cmd)
+            cmd = raw_input()
+            ip, weight = self.__parse_command(cmd)
+            if ip is None and weight is None:
+                pass
 
-            if length == 3 and cmd[0] == 'add':
-                try:
-                    ip = cmd[1]
-                    weight = int(cmd[2])
-                    if not self.__check_ip(ip):
-                        print('Invalid IP. Try again.')
-                except Exception as e:
-                    print(e)
-                    sys.exit(1)
-            elif length == 2 and cmd[0] == 'del':
+    def __parse_command(self, cmd_input):
+        cmd = cmd_input.split(' ')
+        length =  len(cmd)
+        ip     = None
+        weight = None
+
+        if length == 3 and cmd[0] == 'add':
+            try:
                 ip = cmd[1]
+                weight = int(cmd[2])
                 if not self.__check_ip(ip):
                     print('Invalid IP. Try again.')
-            else:
-                print('Invalid command. Try again.')
+            except Exception as e:
+                print(e)
+                sys.exit(1)
+        elif length == 2 and cmd[0] == 'del':
+            ip = cmd[1]
+            if not self.__check_ip(ip):
+                print('Invalid IP. Try again.')
+        else:
+            print('Invalid command. Try again.')
+        return ip, weight
 
     def __check_ip(self, ip):
         b = ip.split('.')
         if len(b) != 4:
             return False
-
         for elem in b:
             try:
                 if int(elem) < 0 or int(elem) > 255:
                     return False
             except Exception as e:
                 return False
-
         return True
