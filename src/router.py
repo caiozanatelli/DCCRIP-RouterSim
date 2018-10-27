466c9cab220c8ac4f04aef8e0ffcfb6c526a5c54
Caio Zanatelli <caio.zanatelli@dcc.ufmg.br>
2018-10-27 15:48:54 -0300

    Adding stdin and udp selectors to handle operations

--- src/router.py
+++ src/router.py
@@ -1,31 +1,72 @@
+import selectors
 import socket
 import sys
 import re
+from threading import Lock, Timer
 
 DEFAULT_PORT   = 55151
 DEFAULT_PERIOD = 30
 
 class Router:
-    __ip     = None
-    __period = DEFAULT_PERIOD
-    __port   = DEFAULT_PORT
-    __sock   = None
 
-    def __init__(self, ip, period):
-        self.__ip     = ip
-        self.__period = period
+    def __init__(self, addr, period=None):
+        self.__addr   = addr
+        self.__period = period if period is not None else DEFAULT_PERIOD
         self.__port   = DEFAULT_PORT
         self.__sock   = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.__sock.bind((self.__ip, self.__port))
+        self.__sock.bind((self.__addr, self.__port))
+        self.__routes = dict()
+        self.__links  = dict()
+        # Setting the selector for input (stdin and udp)
+        self.__selector = selectors.DefaultSelector()
+        self.__selector.register(sys.stdin, selectors.EVENT_READ, self.__handle_command)
+        self.__selector.register(self.__sock, selectors.EVENT_READ, self.__handle_message)
+        # Setting the locks to critical sessions
+        self.__routes_lock = Lock()
+        self.__links_lock  = Lock()
 
     def run(self):
         while True:
-            cmd = raw_input()
-            ip, weight = self.__parse_command(cmd)
-            if ip is None and weight is None:
-                pass
+            #cmd = raw_input()
+            evts = self.__selector.select()
+            for key, mask in evts:
+                callback_funct = key.data
+                if key.fileobj == sys.stdin:
+                    cmd = input()
+                    callback_funct(cmd)
+                elif key.fileobj == self.__sock:
+                    print('Message handler not implemented yet. Sorry.')
 
-    def __parse_command(self, cmd_input):
+            #ip, weight = self.__parse_command(cmd)
+            #if ip is None and weight is None:
+            #    pass
+
+    def add_link(self, addr, weight):
+        if addr == self.__addr:
+            print('Error on adding link: router trying to make a link to itself.')
+            sys.exit(1)
+        self.__links_lock.acquire()
+        self.__links[addr] = weight
+        self.__links_lock.release()
+
+    def remove_link(self, addr):
+        # First we safely remove the addr from the routing table
+        self.__routes_lock.acquire()
+        for key, mask in self.__routes:
+            try:
+                #TODO
+                pass
+            except:
+                pass
+        self.__routes_lock.release()
+
+        # Then we safely remove the addr from the link table
+        self.__links_lock.acquire()
+        del self.__links[addr]
+        self.__links_lock.release()
+
+    def __handle_command(self, cmd_input):
+        # TODO: perform all the commands in this function
         cmd = cmd_input.split(' ')
         length =  len(cmd)
         ip     = None
@@ -47,6 +88,10 @@ class Router:
         else:
             print('Invalid command. Try again.')
         return ip, weight
+
+    def __handle_message(self):
+        # TODO: handle all the message types we receive by UDP
+        pass
 
     def __check_ip(self, ip):
         b = ip.split('.')
